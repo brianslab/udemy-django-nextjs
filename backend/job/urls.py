8e313da7a9c5a1a941fa50ac8adf2a353408a5dd
brianslab <bdkuhn602@gmail.com>
2023-05-27 04:48:10 -0700

    create and edit jobs

--- backend/job/urls.py
+++ backend/job/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('jobs/', views.getAllJobs, name='jobs'),
+    path('jobs/new/', views.newJob, name='newJob'),
     path('jobs/<str:pk>/', views.getJob, name='job'),
+    path('jobs/<str:pk>/edit/', views.editJob, name='editJob'),
 ]
