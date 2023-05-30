8eed7b635e01a212abfd317c02b64f6258b2f2cf
brianslab <bdkuhn602@gmail.com>
2023-05-30 00:56:28 -0700

    user login

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('job.urls')),
-    path('api/', include('account.urls'))
+    path('api/', include('account.urls')),
+    path('api/token', TokenObtainPairView.as_view()),
+    path('api/token/verify', TokenVerifyView.as_view())
 ]
