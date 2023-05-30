ce226e694798a3214306d9a04924ce3d9bf4e652
brianslab <bdkuhn602@gmail.com>
2023-05-30 00:48:34 -0700

    user registration

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.contrib.auth.hashers import make_password
+from .serializers import SignUpSerializer, UserSerializer
+
+from rest_framework.permissions import IsAuthenticated
+
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+@api_view(['POST'])
+def register(request):
+    data = request.data
+
+    user = SignUpSerializer(data=data)
+
+    if user.is_valid():
+        if not User.objects.filter(email=data['email']).exists():
+            user = User.objects.create(
+                first_name=data['first_name'],
+                last_name=data['last_name'],
+                username=data['username'],
+                email=data['email'],
+                password=make_password(data['password'])
+            )
+
+            return Response({
+                'message': 'User registered.'},
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response({
+                'error': 'Email already in use.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    else:
+        return Response(user.errors)
